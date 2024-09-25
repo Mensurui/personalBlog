f345b5a84e9bc0446e7edd1035ee7003094bdff8
Mensurui <mensurk3000@gmail.com>
2024-09-25 17:20:21 +0300

    Database Connected Successfully

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"net/http"
 )
@@ -15,9 +18,20 @@ type application struct {
 }
 
 func main() {
+	dns := flag.String("dns", "postgres://web:pass@localhost:5432/personalblog", "Postgres Datasource Name")
+
+	flag.Parse()
+	db, err := openDB(*dns)
+
+	if err != nil {
+		log.Println("Connection error: %v", err)
+		return
+	}
+	log.Println("Connection working")
+	defer db.Close()
 	templateCache, err := newTemplateCache()
 	if err != nil {
-		log.Println("error")
+		log.Printf("error: %v", err)
 	}
 
 	app := &application{
@@ -27,4 +41,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func openDB(dns string) (*pgxpool.Pool, error) {
+	dbPool, err := pgxpool.New(context.Background(), dns)
+	if err != nil {
+		return nil, err
+	}
+	if err = dbPool.Ping(context.Background()); err != nil {
+		dbPool.Close()
+		return nil, err
+	}
+	return dbPool, nil
 }
